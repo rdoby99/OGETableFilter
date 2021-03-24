3b14d6aa091695e327c9953abd0be5f2e126e7e3
rdoby99 <74220362+rdoby99@users.noreply.github.com>
2021-03-24 09:24:55 -0400

    Updated files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const selectorContainer = document.querySelector("#selectors");
 const trs = document.querySelectorAll("tr");
 const selectors = document.querySelectorAll("select")
 
+// Filters rows based on table filters
 selectorContainer.addEventListener('change', (e)=> {
     e.preventDefault;
     const region = document.querySelector("#regionSelect").value;
@@ -9,6 +10,7 @@ selectorContainer.addEventListener('change', (e)=> {
     const funding = document.querySelector("#sourceSelect").value;
     const level = document.querySelector("#levelSelect").value;
     const amount = document.querySelector("#amountSelect").value;
+    const college = document.querySelector("#collegeSelect").value;
 
 
     for (let row of trs) {
@@ -37,15 +39,42 @@ selectorContainer.addEventListener('change', (e)=> {
       if (row.dataset[4]>amount && amount!=="any" && row.dataset[4]!=="varies" && amount!=="1001") {
         row.style.display = "none";
       }
+
+      if (row.dataset[5]!==college && college!=="any" && row.dataset[5]!=="anycol") {
+        row.style.display = "none";
+      }
     }
     }
 });
 
+// Reset Button
+document.querySelector('#resetButton').addEventListener('click', () => {
+  for (let row of trs) {
+    if (row===trs[0]) {
+
+    } else {
+    row.removeAttribute("style");
+    }
+  }
+
+  const val = "any";
+  for (let select of selectors) {
+    const opts = select.options;
+    for (j = 0; opt = opts[j]; j++) {
+      if (opt.value == val) {
+        select.selectedIndex = j;
+        break;
+      }
+  }
+  }
+})
+
+// Creates data attributes for rows based on tag column
 function dynamicTags() {
   
 for (let row of trs) {  
   const rowTags = row.lastElementChild.textContent;
-  const rowTagsArr = rowTags.split(",");
+  const rowTagsArr = rowTags.toLowerCase().replace(/\s/g, "").split(",");
     
   for(i=0; i < rowTagsArr.length; i++) {
     row.setAttribute("data-" + i, rowTagsArr[i]);
